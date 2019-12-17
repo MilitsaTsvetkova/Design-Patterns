d81f1bb98f5d0167ecd6d4e2e39f72a4137177b5
MilitsaTsvetkova <militsa.tsvetkova@softwaregroup.com>
2019-12-18 00:27:28 +0200

    added enum class,made some corrections

--- Creational Design Patterns/src/com/company/FactoryDesignPattern/ComputerClient.java
+++ Creational Design Patterns/src/com/company/FactoryDesignPattern/ComputerClient.java
@@ -1,12 +1,13 @@
 package com.company.FactoryDesignPattern;
 
 import com.company.FactoryDesignPattern.factory.ComputerFactory;
+import com.company.FactoryDesignPattern.factory.ComputerType;
 import com.company.FactoryDesignPattern.model.Computer;
 
-public class TestFactory {
+public class ComputerClient {
     public static void main(String[] args) {
-        Computer pc = ComputerFactory.getComputer("PC","2 GB","500 GB","2.4 GHz");
-        Computer server = ComputerFactory.getComputer("Server","16 GB","1 TB","2.9 GHz");
+        Computer pc = ComputerFactory.getComputer(ComputerType.PC,"2 GB","500 GB","2.4 GHz");
+        Computer server = ComputerFactory.getComputer(ComputerType.Server,"16 GB","1 TB","2.9 GHz");
         System.out.println("Factory PC Configuration: "+ pc);
         System.out.println("Factory Server Configuration: "+ server);
     }
